ace832680ac6c9d7a073080638770c14753db012
shdomi8599 <117655658+shdomi8599@users.noreply.github.com>
2023-09-07 17:24:17 +0900

    [level 2] Title: 중복 제거하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/59408. 중복 제거하기/중복 제거하기.sql
+++ 프로그래머스/lv2/59408. 중복 제거하기/중복 제거하기.sql
@@ -1,2 +1,2 @@
 -- 코드를 입력하세요
-select count(*) from (select count(*) from ANIMAL_INS where name is not null group by name) as a
+select count(*) from (select * from ANIMAL_INS where name is not null group by name) as a
